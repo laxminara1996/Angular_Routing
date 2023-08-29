573298f037cdcbc92f9b7cca2de7461a2ea557a0
laxminara1996 <laxminara1996@gmail.com>
2023-08-29 19:49:19 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { ServiceComponent } from './service/service.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const myRoutes:Routes=[
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'home',component:HomeComponentComponent},
+  {path:'service',component:ServiceComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    LoginComponent,
+    RegisterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
